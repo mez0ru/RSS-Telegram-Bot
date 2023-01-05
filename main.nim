bc13e7a18d44a3ef211b30a2865096819897cee8
Hamzah Al-washali <32926868+mez0ru@users.noreply.github.com>
2023-01-05 18:03:36 +0800

    Big update 0.2.0
    Some changes are unfortunately private, like the changes I made to my fork of telebot.
    However, now it should work even better and more efficiently than before.
    migrated to easy_sqlite3 for better compatibility with --mm:orc

--- main.nim
+++ main.nim
@@ -1,11 +1,13 @@
-import std/db_sqlite except open
-import telebot, asyncdispatch, logging, options, strutils
-import strformat
+from telebot import Telebot, newTeleBot, onCommand, sendMessage, repeat, poll, Command
+import asyncdispatch
+import logging, strutils
+from strformat import fmt
 import httpClient
-import streams, xmlparser, xmltree
+from streams import newStringStream
+import xmlparser, xmltree
 import tables
-import CustomizedHttpClient
-import std/locks
+from std/locks import Lock, withLock, initLock
+import easy_sqlite3
 
 var L = newConsoleLogger(fmtStr="$levelname, [$time] ")
 addHandler(L)
@@ -16,181 +18,174 @@ template hold(lock: Lock, body: untyped) =
     withLock lock:
       body
 
-let pool = newAsyncHttpClientPool(2)
-
-const API_KEY = strip("YOUR API KEY")
+const API_KEY = strip "YOUR API KEY"
 const test_rss = "https://www.reddit.com/r/funny/new/.rss"
 
-const chatId: int64 = 0 # Your chatID if you'd like to make it private for yourself only.
-const delay = 1
-const predicates = {"URL CONDITION": ["*contains this*", "*or maybe this*"]}.toTable()
-var lastLinks: Table[int, seq[string]]
-var toBeSentNotifications: seq[string]
+const chatId: int64 = 67345084
+const delay: int64 = 28 * 60
+const predicates = {"rss url": ["list of conditions"]}.toTable()
+
+var lastLinks: Table[int64, seq[string]]
+
 var lastLinksLock,
-    poolLock,
-    toBeSentNotificationsLock
+    dbLock,
     : Lock
+
 lastLinksLock.initLock()
-poolLock.initLock()
-toBeSentNotificationsLock.initLock()
+dbLock.initLock()
 
-let db = db_sqlite.open("data.db", "", "", "")
+var db = initDatabase("data.db")
 
-let bot = newTeleBot(API_KEY)
+proc CreateRSS(name, link: string, etag = "") {.importdb: """
+  INSERT INTO rss(name, link, etag) VALUES ($name, $link, $etag);
+""".}
 
+proc RemoveRSS(name: string) {.importdb: """
+  DELETE FROM rss WHERE name = $name;
+""".}
 
-proc getAllRSS(): seq[Row] =
-    return db.getAllRows(sql"SELECT * FROM rss")
+iterator iterate_rss_full(): tuple[id: int, name: string, link: string] {.importdb: """
+  SELECT id, name, link FROM rss;
+""".} = discard
 
-proc CreateRSS(name, link: string, etag = ""): bool =
-    db.tryExec(sql"INSERT INTO rss('name','link','etag') VALUES(?,?,?)", name, link, etag)
+iterator iterate_rss_names(): tuple[id: int, name: string] {.importdb: """
+  SELECT id, name FROM rss;
+""".} = discard
 
-proc RemoveRSS(name: string): bool =
-  db.tryExec(sql"DELETE FROM rss WHERE name = ?", name)
+iterator search_rss(query: string): tuple[id: int, name: string] {.importdb: """
+  SELECT id, name FROM rss WHERE name LIKE $query;
+""".} = discard
 
-proc init_sqlite() =
-    db.exec(sql"""
-      CREATE TABLE IF NOT EXISTS rss(
-        id INTEGER NOT NULL PRIMARY KEY,
-        name text NOT NULL,
-        link text NOT NULL,
-        etag text,
-        created_at timestamp NOT NULL DEFAULT current_timestamp,
-        updated_at timestamp NOT NULL DEFAULT current_timestamp
-      );
-	  
-      CREATE TABLE IF NOT EXISTS urlCondition (
-          id INTEGER NOT NULL PRIMARY KEY,
-          condition text NOT NULL
-      );
-      
-      CREATE TABLE IF NOT EXISTS contentCondition (
-          id INTEGER NOT NULL PRIMARY KEY,
-          condition text NOT NULL,
-          url_condition_id INTEGER NOT NULL,
-          FOREIGN KEY (url_condition_id) REFERENCES urlCondition(id) ON DELETE CASCADE
-      );
+proc init_sqlite() {.importdb: """
+  CREATE TABLE IF NOT EXISTS rss(
+    id INTEGER NOT NULL PRIMARY KEY,
+    name text NOT NULL,
+    link text NOT NULL,
+    etag text,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp
+  );
 
-      CREATE TRIGGER IF NOT EXISTS UpdateLastTime UPDATE OF name, link, etag ON rss
-      BEGIN
-        UPDATE rss SET updated_at=CURRENT_TIMESTAMP WHERE id=NEW.id;
-      END;""")
+  CREATE TABLE IF NOT EXISTS urlCondition (
+      id INTEGER NOT NULL PRIMARY KEY,
+      condition text NOT NULL
+  );
+  
+  CREATE TABLE IF NOT EXISTS contentCondition (
+      id INTEGER NOT NULL PRIMARY KEY,
+      condition text NOT NULL,
+      url_condition_id INTEGER NOT NULL,
+      FOREIGN KEY (url_condition_id) REFERENCES urlCondition(id) ON DELETE CASCADE
+  );
 
-proc containsX(text: string, arr: openArray[string]): bool =
+  CREATE TRIGGER IF NOT EXISTS UpdateLastTime UPDATE OF name, link, etag ON rss
+  BEGIN
+    UPDATE rss SET updated_at=CURRENT_TIMESTAMP WHERE id=NEW.id;
+  END;
+""".}
+
+proc contains(text: string, arr: openArray[string]): bool =
   for x in arr:
-    if text.toLower().contains(x):
+    if text.toLowerAscii().contains(x):
       return true
   result = false
 
-proc formAMessage(name, title, link: string): string =
-  fmt"RSS: {name}{'\n'}Title: {title}{'\n'}{link}"
+proc formAMessage(name, link: string): string =
+  fmt"RSS: {name}{'\n'}{link}"
 
-proc monitorRSS(oneTime: bool, b: Telebot) {.gcsafe, async.} =
-  while true:
-    let allData = getAllRSS()
-    for x in allData:
+proc monitorRSS(b: Telebot, isRepeat: bool): Future[bool] {.gcsafe, async.} =
+  var toBeSentNotifications: seq[string]
+  let client = newAsyncHttpClient()
+  echo fmt"run with {$isRepeat}"
+  hold dbLock:
+    for id, name, rssLink in db.iterate_rss_full():
       # Get Xml content from rss link
-      let id = parseInt(x[0])
-      var rssContent: string
-      hold poolLock:
-        let client = await pool.dequeue()
-        defer: pool.enqueue client
-        rssContent = await client.getContent(x[2])
+      let rssContent = await client.getContent(rssLink)
+      client.close()
       # Parse XML content
       let root= parseXML(newStringStream(rssContent))
 
-      # Find feeds using the tag "entry"
+      # Find feeds using the tag "entry", grab last 2s.
       let entries = root.findAll("entry")[0..1]
 
-      # Get latest 5 feeds
-      let lastEntry = entries[0].child("link").attr("href")
+      let lastLink = entries[0].child("link").attr("href")
       
       hold lastLinksLock:
         if lastLinks.hasKey(id):
-          if lastLinks[id].contains(lastEntry):
+          if lastLinks[id].contains(lastLink):
             continue
           else:
             lastLinks[id].setLen(0)
-            lastLinks[id].add(lastEntry)
+            lastLinks[id].add(lastLink)
         else:
-          lastLinks.add(id, @[lastEntry])
+          lastLinks[id] = @[lastLink]
         
       var key: string
       for y in predicates.keys:
-        if x[2].contains(y):
+        if rssLink.contains(y):
           key = y
           break
-      # 2 entries is probably enough but you can go furthur
+
       for i, y in entries[0..^1]:
-        if containsX(y.child("title").innerText, predicates[key]) or containsX(y.child("media:group").child("media:description").innerText, predicates[key]):
-          let title = y.child("title").innerText
+        if predicates[key] in y.child("title").innerText or predicates[key] in y.child("media:group").child("media:description").innerText:
           if i != 0:
             let link = y.child("link").attr("href")
-            hold toBeSentNotificationsLock:
-              toBeSentNotifications.add(formAMessage(x[1], title, link))
+            toBeSentNotifications.add formAMessage(name, link)
             hold lastLinksLock:
-              if not lastLinks[id].contains(link):
+              if not lastLinks[id].contains link:
                 lastLinks[id].add(link)
           else:
-            hold toBeSentNotificationsLock:
-              toBeSentNotifications.add(formAMessage(x[1], title, lastEntry))
+            toBeSentNotifications.add formAMessage(name, lastLink)
 
-    hold toBeSentNotificationsLock:
-      if oneTime and toBeSentNotifications.len == 0:
-        discard await b.sendMessage(chatId, "Nothing new 😖", parseMode = "markdown", disableNotification = false)
-        return
+  if not isRepeat and toBeSentNotifications.len == 0:
+    discard await b.sendMessage(chatId, "Nothing new 😖", parseMode = "markdown", disableNotification = false)
+    return
 
-      for x in toBeSentNotifications:
-        discard await b.sendMessage(chatId, x, disableNotification = false)
+  for x in toBeSentNotifications:
+    discard await b.sendMessage(chatId, x, disableNotification = false)
 
-      toBeSentNotifications.setLen(0)
+  toBeSentNotifications.setLen(0)
 
-    echo "Finished a test cycle"
-    await sleepAsync(delay * 60 * 1000)
+  echo "Finished a test cycle"
 
 proc listRSSCommand(b: Telebot, c: Command): Future[bool] {.gcsafe, async.} =
-  let data = getAllRSS()
-  if data.len != 0:
-    for row in data[0..^1]:
-      let id = parseInt(row[0])
+  var noRecord = true
+  hold dbLock:
+    for id, name in db.iterate_rss_names:
       hold lastLinksLock:
         if lastLinks.hasKey(id) and lastLinks[id].len != 0:
-          discard await b.sendMessage(chatId, fmt"Name: {row[1]}{'\n'}Last RSS: {lastLinks[id][0]}")
-  else:
+          discard await b.sendMessage(chatId, fmt"Name: {name}{'\n'}Last RSS: {lastLinks[id][0]}")
+      noRecord = false
+  if not noRecord:
     discard await b.sendMessage(chatId, "List is empty :/")
   result = true
 
 proc removeRSSCommand(b: Telebot, c: Command): Future[bool] {.gcsafe, async.} =
   if not c.params.isEmptyOrWhitespace:
-    if RemoveRSS(strip(c.params)):
-      discard await b.sendMessage(chatId, fmt"Successfully removed {c.params}")
-    else:
-      discard await b.sendMessage(chatId, "Did not remove, database returned an error.")
+    hold dbLock:
+      db.RemoveRSS(strip(c.params))
+    discard await b.sendMessage(chatId, fmt"Successfully removed {c.params}")
   else:
     discard await b.sendMessage(chatId, "No name provided, *nothing* is _done_.", parseMode = "markdown")
   result = true
 
 proc addRSSCommand(b: Telebot, c: Command): Future[bool] {.gcsafe, async.} =
   if c.params.contains(" "):
-    let rss = c.params.split(' ')
-    if CreateRSS(rss[0], rss[1]):
-      discard await b.sendMessage(chatId, fmt"Successfully added {rss[0]}")
-    else:
-      discard await b.sendMessage(chatId, "Did not remove, database returned an error.")
+    let rss = splitWhitespace c.params
+    hold dbLock:
+      db.CreateRSS(rss[0], rss[1])
+    discard await b.sendMessage(chatId, fmt"Successfully added {rss[0]}")
   else:
     discard await b.sendMessage(chatId, "Wrong command, make sure you provide *BOTH* the _name_ and the _feed link_.\nExample:\n/add My-feed https://example.com/feed.xml", parseMode = "markdown")
   result = true
 
-proc updateRSSCommand(b: Telebot, c: Command): Future[bool] {.gcsafe,async.} =
-  await monitorRSS(true, b)
+proc updateRSSCommand(b: Telebot, c: Command): Future[bool] {.gcsafe, async.} =
+  discard await monitorRSS(b, false)
 
 proc testRSSCommand(b: Telebot, c: Command): Future[bool] {.gcsafe, async.} =
-  var rssContent: string
-  hold poolLock:
-    let client = await pool.dequeue()
-    defer: pool.enqueue client
-    rssContent = await client.getContent(test_rss)
-  
+  let client = newAsyncHttpClient()
+  let rssContent = await client.getContent(test_rss)
+  client.close()
   let root= parseXML(newStringStream(rssContent))
 
   # get the first "entry"
@@ -199,15 +194,31 @@ proc testRSSCommand(b: Telebot, c: Command): Future[bool] {.gcsafe, async.} =
   let entryTitle = entry.child("title").innerText
   discard await b.sendMessage(chatId, fmt"Title: {entryTitle}{'\n'}{entryLink}", disableNotification = false)
 
+proc getRSSCommand(b: Telebot, c: Command): Future[bool] {.gcsafe, async.} =
+  var noRecord = true
+  let query = '%' & c.params & '%'
+  hold dbLock:
+    for id, name in db.search_rss(query):
+      hold lastLinksLock:
+        if lastLinks.hasKey(id):
+          for link in lastLinks[id]:
+            discard await b.sendMessage(chatId, fmt"Name: {name}{'\n'}Last RSS: {link}")
+            noRecord = false
+  if noRecord:
+    discard await b.sendMessage(chatId, "No records found, try again...")
 
-init_sqlite()
 
+db.init_sqlite()
 
+let bot = newTeleBot(API_KEY)
+
+discard monitorRSS(bot, true)
 
 bot.onCommand("list", listRSSCommand)
 bot.onCommand("remove", removeRSSCommand)
 bot.onCommand("add", addRSSCommand)
+bot.onCommand("get", getRSSCommand)
 bot.onCommand("update", updateRSSCommand)
 bot.onCommand("test", testRSSCommand)
-discard monitorRSS(false, bot)
+bot.repeat(delay, monitorRSS)
 bot.poll(timeout=300)
